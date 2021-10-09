68d4398bc36bf23946a61982732e73f7c15e866d
salavat876 <mavlikhanov2002@mail.ru>
2021-10-09 11:19:04 +0400

    add thunks

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
-import {compose, createStore} from "redux";
+import {applyMiddleware, compose, createStore} from "redux";
 import {rootReducer} from "./reducers/rootReducer";
 import {loadState, saveState} from "./localStorage/localStorage";
+import thunk from "redux-thunk";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const state = loadState();
-export const store = createStore(rootReducer,state,composeEnhancers());
+export const store = createStore(rootReducer,state,composeEnhancers(applyMiddleware(thunk)));
 store.subscribe(()=>{
     saveState()
 });
